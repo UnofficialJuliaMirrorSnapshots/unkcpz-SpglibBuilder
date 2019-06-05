5bca6b551f1a8d3d2fb77e64aac849bf2be13d24
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-06-05 02:47:31 +0000

    Snapshot of branch master taken on 2019-06-04T22:47:30.51-04:00 from "https://github.com/UnofficialJuliaMirror/unkcpz-SpglibBuilder"

---
+++ README.md
@@ -0,0 +1,9 @@
+# SpglibBuilder
+
+[![Build Status](https://travis-ci.org/unkcpz/SpglibBuilder.svg?branch=master)](https://travis-ci.org/unkcpz/SpglibBuilder)
+
+This repository builds binary artifacts for the SpglibBuilder project. Binary artifacts are automatically uploaded to
+[this repository's GitHub releases page](https://github.com/unkcpz/SpglibBuilder/releases) whenever a tag is created
+on this repository.
+
+This repository was created using [BinaryBuilder.jl](https://github.com/JuliaPackaging/BinaryBuilder.jl)
---
+++ build_tarballs.jl
@@ -0,0 +1,49 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "SpglibBuilder"
+version = v"1.12.2"
+
+# Collection of sources required to build SpglibBuilder
+sources = [
+    "https://github.com/atztogo/spglib/archive/v1.12.2.tar.gz" =>
+    "d92f5e4fa0f54cc0abd0209b81c4d5c647dae9d25b774c2296f44b8558b17976",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd spglib-1.12.2/
+mkdir _build
+cd _build/
+cmake -DCMAKE_INSTALL_PREFIX=$prefix \
+      -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain \
+      ../
+make
+make install VERBOSE=1
+"""
+
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    MacOS(:x86_64),
+    Linux(:x86_64, libc=:glibc),
+    FreeBSD(:x86_64),
+    Linux(:i686, libc=:glibc)
+]
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libsymspg", :libsymspg)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
