180b26f3307372c2c6844ff2a20e3c7750a13221
UnofficialJuliaMirrorBot <43731649+UnofficialJuliaMirrorBot@users.noreply.github.com>
2019-07-18 18:33:53 +0000

    Snapshot of branch master taken on 2019-07-18T14:33:52.367-04:00 from "https://github.com/UnofficialJuliaMirror/unkcpz-SpglibBuilder"

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -3,19 +3,19 @@
 using BinaryBuilder
 
 name = "SpglibBuilder"
-version = v"1.12.2"
+version = v"1.13.0"
 
 # Collection of sources required to build SpglibBuilder
 sources = [
-    "https://github.com/atztogo/spglib/archive/v1.12.2.tar.gz" =>
-    "d92f5e4fa0f54cc0abd0209b81c4d5c647dae9d25b774c2296f44b8558b17976",
+    "https://github.com/atztogo/spglib/archive/v1.13.0.tar.gz" =>
+    "ed72ae7bdd129487c45ff7bebb8e2ac03074657060e068b015e7741c0271e16b",
 
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd spglib-1.12.2/
+cd spglib-1.13.0/
 if [[ ${target} == *-mingw32 ]]; then
     sed -i -e 's/LIBRARY/RUNTIME/' CMakeLists.txt
 fi
